33d632917035d7ad0df655ffd1493a1623484171
David Castaneda <Davidcas7@hotmail.com>
2026-03-03 13:53:18 -0500

    Initial Commit

---
+++ BeRealClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeRealClone
+//
+//  Created by David Castaneda on 3/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
